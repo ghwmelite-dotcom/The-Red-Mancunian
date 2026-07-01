ff8600a557641ad3bed327e293c68cd3196d19fb
ghwmelite-dotcom <ghwmelite@gmail.com>
2026-07-01 23:40:31 +0000

    feat(matchsim): title-safe header + symmetric scoreboard anchors

--- tiktok/matchsim/layout.py
+++ tiktok/matchsim/layout.py
@@ -8,19 +8,21 @@ MARGIN = 64
 
 
 def live_zones():
-    header = (MARGIN, 40, W - 2 * MARGIN, 70)
-    scoreboard = (MARGIN, 130, W - 2 * MARGIN, 130)
-    progress = (MARGIN, 280, W - 2 * MARGIN, 14)
-    winprob = (MARGIN, 320, W - 2 * MARGIN, 90)
+    header = (MARGIN, 96, W - 2 * MARGIN, 70)
+    scoreboard = (MARGIN, 186, W - 2 * MARGIN, 130)
+    progress = (MARGIN, 336, W - 2 * MARGIN, 14)
+    winprob = (MARGIN, 376, W - 2 * MARGIN, 96)
 
-    arena_top = 470
+    arena_top = 540
     arena_bottom = H - 250
     r = min((W - 2 * MARGIN) // 2, (arena_bottom - arena_top) // 2)
     cx = W // 2
     cy = arena_top + r
 
+    score_anchors = (W // 2 - 210, W // 2, W // 2 + 210)
     caption = (MARGIN, H - 210, W - 2 * MARGIN, 90)
     return {
         "header": header, "scoreboard": scoreboard, "progress": progress,
         "winprob": winprob, "arena": (cx, cy, r), "caption": caption,
+        "score_anchors": score_anchors,
     }
